274b47a02134db912ad3586e6c3dd7e1facd6882
Khalid Lamachi <khalidlamachi2005@gmail.com>
2026-05-31 21:51:14 +0100

    Ajout des tables équipes et appartenance_equipe

--- Script de création de la base de données de MiniJira.sql
+++ Script de création de la base de données de MiniJira.sql
@@ -13,4 +13,18 @@ CREATE TABLE utilisateurs (
     type_utilisateur VARCHAR(50)
 );
 
+CREATE TABLE equipes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(100) NOT NULL,
+    objectif TEXT
+);
+
+CREATE TABLE appartenance_equipe (
+    id_equipe INT,
+    id_utilisateur INT,
+    PRIMARY KEY(id_equipe, id_utilisateur),
+    FOREIGN KEY(id_equipe) REFERENCES equipes(id),
+    FOREIGN KEY(id_utilisateur) REFERENCES utilisateurs(id)
+);
+
 select * from utilisateurs;
